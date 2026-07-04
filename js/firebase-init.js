9e88085b2dd27a731f2824ac3b081878ca98a6c8
t1072959-ops <t1072959@gmail.com>
2026-07-04 14:15:46 +0800

    Add files via upload

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: "store-46293",
   storageBucket: "store-46293.firebasestorage.app",
   messagingSenderId: "581356069497",
-  appId: "1:581356069497:web:3b5eccd2128b90cdb46aa8"
+  appId: "1:581356069497:web:3b5eccd2128b90cdb46aa8",
 };
 
 // 後台登入密碼(簡易保護,不是高安全性,適合內部使用)
